19c13fba964e5cf82e01e0a419ec1c955808c348
dimisjim <dimitris.moraitidis@gmail.com>
2018-06-24 16:50:40 +0200

    added first script

---
+++ addFirstLineToCSV.ps1
@@ -0,0 +1,7 @@
+﻿Get-ChildItem "C:\Users\jim\files\ds1" -Filter *.csv | 
+Foreach-Object {
+
+    $headerToAdd = "id,channel,seller,int_rate,orig_upb,loan_term,start_date,first_pay_date,ltv,cltv,borrow_numb,dti,borrow_credit_score,buyer_indic,purpose,prop_type,nb_units,occ_status,prop_state,zip,insurance_perc,prod_type,coborrow_credit_score,insurance_type,reloc_indic"
+
+    $headerToAdd + "`n" + (Get-Content $_.FullName -Raw) | Set-Content $_.FullName
+}
